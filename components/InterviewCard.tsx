f68f5dbbbaf5088c333cf89da93a7202af9da6f2
Aryan2600 <aryanpatidar260@gmail.com>
2025-11-03 20:17:14 +0530

    show feedback

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -4,11 +4,14 @@ import {getRandomInterviewCover} from "@/lib/utils";
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "@/components/DisplayTechIcons";
+import {getFeedbackByInterviewId} from "@/lib/actions/general.action";
 
-const InterviewCard = ({id,userId,role,type,techstack,createdAt}:InterviewCardProps) => {
-    const feedback = null as Feedback | null;
+const InterviewCard = async  ({id,userId,role,type,techstack,createdAt}:InterviewCardProps) => {
+    const feedback = userId && id
+    ? await getFeedbackByInterviewId({interviewId: id , userId})
+        : null ;
     const normalizedType =/mix/gi.test(type) ? 'Mixed' : type;
-    const formatteddate = dayjs(feedback?.createdAt || createdAt || Date.now() ).format('MMM D, YYYY');
+    const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now() ).format('MMM D, YYYY');
 
     return (
         <div className="card-border w-[360px max-sm:w-full min-h-96]" >
